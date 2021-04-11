dab00a53be4d0e7a8422e8fdfd481fb6bbac7aa5
Nitro Team <nirtoteam323@gmail.com>
2021-04-11 11:23:31 +0430

    main project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Menu from '../src/components/menu/menu'
+import Home from '../src/layouts/home/home'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Menu />
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/menu/menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './menu.css';
+
+function Menu(props) {
+    return (
+        <div>
+            Menu
+        </div>
+    )
+}
+
+export default Menu
